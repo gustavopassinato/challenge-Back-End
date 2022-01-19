4573ed0c71d314d045e3873f24635d84bf224bdf
Gustavo Passinato Neves <passinato11@gmail.com>
2022-01-19 07:16:36 -0300

    Primeiros arquivos do projeto

---
+++ src/main/java/br/com/FamilyMoney/FamilyMoneyApplication.java
@@ -0,0 +1,13 @@
+package br.com.FamilyMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/FamilyMoney/FamilyMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.FamilyMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
